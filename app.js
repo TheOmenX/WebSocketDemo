3ebc6e0b7bcdffa5dbc7e84bd9e0a8886461ca8f
TheOmenX <tijngiesberts@hotmail.com>
2023-02-16 23:12:55 +0100

    fixed app bug

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 'use strict';
 
-let fs = require('fs');
 let express = require('express');
 let app = express();
 let bodyParser = require('body-parser');
@@ -9,20 +8,7 @@ app.use(bodyParser.json());
 
 // Let's create the regular HTTP request and response
 app.get('/', function(req, res) {
-
-  console.log('Get index');
-  fs.createReadStream('./index.html')
-  .pipe(res);
-});
-
-app.post('/', function(req, res) {
-
-  let message = req.body.message;
-  console.log('Regular POST message: ', message);
-  return res.json({
-
-    answer: 42
-  });
+    res.send('Hello, World!')
 });
 
 module.exports = app;
