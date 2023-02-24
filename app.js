4cd8b229526cff25bbba239b90666c8bd17d3ba9
TheOmenX <tijngiesberts@hotmail.com>
2023-02-24 13:42:49 +0100

    Created demo ws

--- app.js
+++ app.js
@@ -3,12 +3,116 @@
 let express = require('express');
 let app = express();
 let bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const { authenticateToken } = require('./middleware/authentication');
+const dotenv = require('dotenv');
+dotenv.config();
+const jwt_secret = process.env.JWT_SECRET;
+
+const login = [
+    {username: 'admin',
+    password:"$2a$12$YN3ZlUcJRVK4UxyeJ9F4b.0zlCWOzNtNCjl.LSmkPYBoZzhJ39gAi"},
+    {username: 'tijn',
+    password: '$2a$12$ATR8ffhxiBpedE6a5WMLb.jAaNJdTSDgf9MOvUAoEAMioY3NLkJTa'}
+]
+
+const users = [
+    {name: 'Tijn'},
+    {name: 'Joep'},
+    {name: 'Ryan'},
+    {name: 'Roos'},
+    {name: 'Demi'},
+    {name: 'Pepijn'},
+    {name: 'Jet'},
+    {name: 'Floor'},
+    {name: 'Amber'},
+    {name: 'Sieb'},
+]
+
+const catagories = [
+    "Hard Lopers",
+    "Warme Dranken",
+    "Bieren",
+    "Gebak",
+    "Lunch"
+]
+
+const products = [
+    {
+        id: "1",
+        name: "Cappuccino",
+        price: 3.3,
+        catagories: [0,1],
+        ingredients: ["koekje"]
+    },{
+        id: "2",
+        name: "Koffie",
+        price: 2.9,
+        catagories: [0,1],
+        ingredients: ["koekje"]
+    },{
+        id: "3",
+        name: "Tosti",
+        price: 5.5,
+        catagories: [0,4],
+        ingredients: ["Ham", "Kaas", "Curry"]
+    },{
+        id: "4",
+        name: "Appeltaart",
+        price: 4.2,
+        catagories: [0,3],
+        ingredients: []
+    },{
+        id: "5",
+        name: "Grolsch",
+        price: 3.3,
+        catagories: [0,2],
+        ingredients: []
+    },{
+        id: "6",
+        name: "Weizen",
+        price: 2.9,
+        catagories: [2],
+        ingredients: []
+    },{
+        id: "7",
+        name: "Broodje Zalm",
+        price: 5.5,
+        catagories: [4],
+        ingredients: ["Zalm", "Sla", "Roomkaas", "Dille", "Kappertjes"]
+    },{
+        id: "8",
+        name: "Cheesecake",
+        price: 4.2,
+        catagories: [3],
+        ingredients: []
+    }
+]
 
 app.use(bodyParser.json());
 
-// Let's create the regular HTTP request and response
 app.get('/', function(req, res) {
     res.send('Hello, World!')
 });
 
+app.post('/token', async (req,res) => {
+    let token = jwt.sign({ user: req.body.username }, jwt_secret);
+    let pass = login.find(x => x.username == req.body.username)?.password
+    if(pass && await bcrypt.compare(req.body.password, pass)) res.json({token})
+    else res.json()
+})
+
+app.get('/users', authenticateToken, (req,res) => {
+    res.json(users)
+})
+
+app.get('/products', authenticateToken, (req,res) => {
+    res.json(products)
+})
+
+app.get('/catagories', authenticateToken, (req,res) => {
+    res.json(catagories)
+})
 module.exports = app;
--- index.js
+++ index.js
@@ -3,30 +3,64 @@
 let WSServer = require('ws').Server;
 let server = require('http').createServer();
 let app = require('./app')
+const crypto = require("crypto");
 
 let wss = new WSServer({
     server: server
 });
-  
 server.on('request', app);
 
-wss.on('connection', function connection(ws) {
-    console.log('user connected')
+const users = {}
+const activeTables = []
 
-    ws.on('message', function incoming(message) {
+const table = {
+  1234: [
+      {id: 2, name: "Koffie", count: 4, price: 2.9, comment: [], time: Date.now()},
+      {id: 4, name: "Appeltaart", count: 1, price: 4.2, comment: ["Stop"], time: Date.now()}
+    ]
+}
+
+wss.on('connection', function connection(ws) {
+  const id = crypto.randomBytes(8).toString("hex");
+  ws.id = id
+  users[ws.id] = ws
+  console.log(`user connected with id: ${id}`)
+
+
+  ws.on('message', function incoming(message) {
+    console.log(`received: ${message} from ${ws.id} ${ws.table ? `at table ${ws.table}` : ""}`);
+    let data = JSON.parse(message)
+
+    let index = activeTables.findIndex(x => x.table == data.table)
+    if(index != -1) {
+      console.log('table found')
+      if(users[activeTables[index].user].readyState == 1) {
+        ws.send(JSON.stringify({
+        "status": "[ERROR]: Table already exists",
+        //"data": "" Optional
       
-      console.log(`received: ${message}`);
-      
-      ws.send(JSON.stringify({
-  
-        answer: 42
-      }));
-    });
+        }))
+        ws.terminate(); 
+        console.log("Connection terminated, table already in use"); 
+        return
+      }
+    }
+
+    activeTables.push({table: data.table, user: ws.id})
+    users[ws.id].table = data.table
+    ws.send(JSON.stringify(table[data.table]));
   });
-  
-  
-  server.listen(process.env.PORT || 8000, function() {
-  
-    console.log(`http/ws server listening on ${process.env.PORT || 8000}`);
-  });
+
+  ws.on("close", () => {
+    delete users[ws.id]
+    if(ws.table) {
+      let index = activeTables.indexOf(`${ws.table}`)
+      activeTables.splice(index, 1)
+    }
+  })  
+});
+
+server.listen(process.env.PORT || 8000, function() {
+  console.log(`[START]: HTTP/ws server listening on ${process.env.PORT || 8000}`);
+});
 
